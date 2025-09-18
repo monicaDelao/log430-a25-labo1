9151dda0d0ddb6391a5f03cdc8d041bd5a64ed07
monicaDelao <monica-larissa.delao-romero.1@ens.etsmtl.ca>
2025-09-18 19:46:41 -0400

    Labo 1 final - tous les tests passent

--- db-init/init.sql
+++ db-init/init.sql
@@ -1,20 +1,16 @@
--- Créer le tableau Users
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(80) NOT NULL,
     email VARCHAR(80) NOT NULL
 );
 
--- Créer des enregistrements dans Users
 INSERT INTO users (name, email) VALUES
 ('Ada Lovelace', 'alovelace@example.com'),
-('Adele Goldberg', 'agoldberg@example.com'),
 ('Alan Turing', 'aturing@example.com');
-
 
 CREATE TABLE IF NOT EXISTS products (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(80) NOT NULL,
     brand VARCHAR(20) NOT NULL,
-    price DECIMAL(10, 2) NOT NULL
+    price DECIMAL(10,2) NOT NULL
 );
